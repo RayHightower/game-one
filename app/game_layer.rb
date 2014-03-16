46e07b6c3b7366a093cc8f11590bd11e0f0dcbb0
Raymond T. Hightower <rth@wisdomgroup.com>
2014-03-15 21:01:33 -0500

    experimenting with settings

--- app/game_layer.rb
+++ app/game_layer.rb
@@ -1,10 +1,8 @@
 class GameLayer < Joybox::Core::Layer
 
   def on_enter
-
     @player_plane = Sprite.new file_name: 'biplane.jpg',
                                 position: [258, 450] # Screen.half_height]
-
     add_child(@player_plane)
 
     @world = World.new gravity: [0.0, -9.8]
@@ -13,52 +11,35 @@ class GameLayer < Joybox::Core::Layer
       @world.step delta: dt
     end
     
-    body = @world.new_body position: [80, Screen.half_height]
+    body = @world.new_body position: [80, 450]  # Screen.half_height]
 
     @enemy_plane = PhysicsSprite.new file_name: 'blue-biplane.jpg',
                                       body: body
-
     add_child(@enemy_plane)
     
     init_controls
   end
 
   def init_controls
-    
     on_touches_began do | touches, event |
-
       starting_touch = touches.any_object
-
       @starting_touch_location = starting_touch.location
-
     end
 
     on_touches_ended do | touches, event |
-
       end_touch = touches.any_object
-
       end_touch_location = end_touch.location
-
       pumpkin = new_pumpkin_sprite
-
       add_child(pumpkin)
-
       pumpkin.body.apply_force force: (end_touch_location - @starting_touch_location)
-
     end
 
     def new_pumpkin_sprite
-
       pumpkin_body = @world.new_body position: @player_plane.position,
                                       type: KDynamicBodyType
-
       @pumpkin_sprite = PhysicsSprite.new file_name: 'pumpkin.jpg',
                                               body: pumpkin_body
-      
       @pumpkin_sprite
-      
     end
-
-    
   end
 end
--- app/game_scene.rb
+++ app/game_scene.rb
@@ -3,11 +3,9 @@ class GameScene < Joybox::Core::Scene
   def on_enter
 
     background_layer = BackgroundLayer.new
-
     add_child(background_layer)
     
     game_layer = GameLayer.new
-
     add_child(game_layer)
 
   end
